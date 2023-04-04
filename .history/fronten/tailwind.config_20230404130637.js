f2db83a00362567a8c29fcd898c5200bbbe69939
Vasil <vaskodzhakov@gmail.com>
2023-04-04 13:12:52 +0100

    frontend to CRA from Vite
    frontend to CRA from Vite

---
+++ .history/fronten/tailwind.config_20230404130637.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  "plugins": [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss" // MUST come last
+  ],
+  "pluginSearchDirs": false
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,12 @@ import "./App.css"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 )
+
